31e88163c3ed132b975ef1f4b6e5d2475351ed54
edvardP5 <bruhmoment.xd228@gmail.com>
2025-06-08 11:37:41 +0300

    Delete OOP-lab directory
    не ті файли

--- OOP-lab/lab3/src/PrintTimeWithFor.java
+++
@@ -1,9 +0,0 @@
-public class PrintTimeWithFor {
-    public static void main(String[] args) {
-        for (int hour = 0; hour <= 2; hour++) {
-            for (int min = 0; min < 60; min++) {
-                System.out.println(hour + " h " + min + " min");
-            }
-        }
-    }
-}
--- OOP-lab/lab5/src/abc.java
+++
@@ -1,6 +0,0 @@
-public class abc {
-    public static void main(String[] args) {
-        int[] numbers = {3, 7, 2, 9, 5};
-        Main.printIntArray(numbers);
-    }
-}
